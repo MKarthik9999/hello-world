45175bd292a61d742aaebb355772a8437c6ba77b
MKarthik9999 <88875317+MKarthik9999@users.noreply.github.com>
2024-03-30 12:25:49 +0000

    update in code 2nd line

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!, This is my first java example code" );
+        System.out.println( "Hello World!, This is the second line in the java code" );
     }
 }
